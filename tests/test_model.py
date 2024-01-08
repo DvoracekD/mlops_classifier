7154e5e804baf5bedeb59c197f7570e4d8259ba1
dvorado6 <dvorado6@fel.cvut.cz>
2024-01-08 18:00:07 +0100

    add raised error test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,16 @@
 import torch
+import pytest
 from classifier.models.model import MyNeuralNet
 
 def test_model():
     batch_n = 64
-    input = torch.randn(batch_n, 28, 28)
+    input = torch.randn(batch_n, 1, 28, 28)
     model = MyNeuralNet()
     output = model(input)
 
     assert output.shape == (batch_n, 10)
+
+def test_error_on_wrong_shape():
+    with pytest.raises(ValueError, match='Expected input to a 4D tensor'):
+        model = MyNeuralNet()
+        model(torch.randn(1,2,3))
